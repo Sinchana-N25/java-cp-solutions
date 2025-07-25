7fa5f904565f91861b27ef2a9da9be24db65fd5a
Sinchana N <sinchana2505@gmail.com>
2025-07-25 23:40:49 +0530

    Solved 1 easy leetcode problem

---
+++ Easy/p485_MaxConsecutiveOnes.java
@@ -0,0 +1,29 @@
+// LeetCode 485: Max Consecutive Ones
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/max-consecutive-ones/
+package Easy;
+import java.util.*;
+public class p485_MaxConsecutiveOnes{
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+          int most=0;
+          int i,j=0;
+          for(i=0;i<nums.length;i++)
+        {
+           if(nums[i]==1)
+           {
+            j++;
+           }
+        else if(nums[i]==0)
+        { 
+            if(j>most)
+            {
+            most=j;
+            }
+            j=0;
+        }
+        }
+        return Math.max(j,most);
+}
+}
+}
