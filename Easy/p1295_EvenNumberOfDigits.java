7e950705b93ead86e1814e8e912ef45885f51ed0
Sinchana <sinchana2505@gmail.com>
2025-07-27 21:37:48 +0530

    solved easy array problems on leetcode

---
+++ Easy/p1295_EvenNumberOfDigits.java
@@ -0,0 +1,23 @@
+// LeetCode 1295: Find Numbers with Even Number of Digits
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+package Easy;
+import java.util.*;
+public class p1295_EvenNumberOfDigits {
+    class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        String str="";
+       for(int i=0;i<nums.length;i++)
+        {
+            str= Integer.toString(nums[i]);
+            if(str.length()%2==0)
+            {
+                count++;
+            }
+        };
+        return count;
+    }
+}
+}
