6ece7d8de5aaa360702be60b7a56e0c46833ea24
Sinchana <sinchana2505@gmail.com>
2025-08-04 14:09:17 +0530

    Updated p28

--- Easy/p28_IndexOfFirstOccurenceString.java
+++ Easy/p28_IndexOfFirstOccurenceString.java
@@ -7,12 +7,7 @@ package Easy;
 public class p28_IndexOfFirstOccurenceString {
     class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack.contains(needle))
-        {
-          return haystack.indexOf(needle);
-        }
-        else
-        return -1;
+        return haystack.indexOf(needle);
     }
 }
 }
